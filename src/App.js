407afcc556a92e6e44c7355851983f3fe96ed36c
nataliepjlin <nataliepjlin@gmail.com>
2025-03-19 12:39:56 +0800

    switch to react and add header with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <header>
+            <h1><a href="#">購物網站</a></h1>
+            <nav>
+                <ul>
+                    <li><a href="#">首頁</a></li>
+                    <li><a href="#">商品</a></li>
+                    <li><a href="#">關於我們</a></li>
+                    <li><a href="#">聯絡我們</a></li>
+                </ul>
+            </nav>
+            <div className="search-container">
+                <input type="text" placeholder="搜尋商品..." className="search-bar" />
+                <button className="search-btn">search</button>
+            </div>
+        </header>
+    );
 }
-
 export default App;

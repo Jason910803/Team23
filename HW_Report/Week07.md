7728d9d16b9446ffa13e7fb34460702a008b7e05
nataliepjlin <nataliepjlin@gmail.com>
2025-03-28 23:38:28 +0800

    runnable but fail to load contents of products and cart

---
+++ HW_Report/Week07.md
@@ -0,0 +1,11 @@
+# 作業 Week07
+## 練習了哪些當週上課的主題
+- django
+## 額外找了與當週上課的主題相關的程式技術
+- 使用 npm run build結合react和django
+## 組員分工情況
+
+- 林姵均: 使用 npm run build結合react和django
+- 呂學銘: 
+- 鄭絜元: 
+- 郭品謙: 
--- HW_Report/django/backend/urls.py
+++ HW_Report/django/backend/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # API routes go here
+    re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]

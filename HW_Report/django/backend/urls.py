7234850687b73672d3aee57d5d94fea11f58e77e
nataliepjlin <nataliepjlin@gmail.com>
2025-03-29 21:26:29 +0800

    add admin to url to make createsuperuser runnable

--- HW_Report/django/backend/urls.py
+++ HW_Report/django/backend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.urls import path, re_path
 from django.views.generic import TemplateView
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     # API routes go here
+    path('admin/', admin.site.urls),
     re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]
